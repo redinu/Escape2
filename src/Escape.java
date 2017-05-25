b8124bd0243884b124474b835793cc9df66640d3
Rediet Ayalew <rediet.d.ayalew@gmail.com>
2017-05-25 15:26:38 -0400

    Cost Print

---
+++ src/Escape.java
@@ -0,0 +1,14 @@
+
+public class Escape {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Cost\t" + "\tQuantity\t" + "Total");
+		System.out.println("$1,000.00\t" + "4\t" +"\t$4,000.00");
+		System.out.println("$50.00\t" + "\t8\t" + "\t$400.00");
+		System.out.println("__________________________________________");
+		System.out.println("TOTAL:\t" + "\t12\t" + "\t$4,400.00");
+
+	}
+
+}
